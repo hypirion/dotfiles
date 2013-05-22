b6cd3062f5ac172f2efd0bf7d6e67cd10f2baddb
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-05-22 18:39:11 +0200

    Remove border from Full-layout.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Config.Xfce
 import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import qualified Data.Map as M
 
@@ -9,9 +11,14 @@ hypirionTerminal = "xfce-terminal"
 hypirionKeys (XConfig {modMask = mod4Mask}) = M.fromList $
              [((mod4Mask, xK_F4), kill)]
 
+hypirionLayout = tiled ||| Mirror tiled ||| noBorders Full
+  where
+    tiled = Tall 1 (3/100) (1/2)
+
 main = xmonad xfceConfig {
      terminal = hypirionTerminal,
      modMask = mod4Mask,
      handleEventHook = fullscreenEventHook,
+     layoutHook =  avoidStruts $ hypirionLayout,
      keys = \c -> hypirionKeys c `M.union` keys xfceConfig c
      }
