9695f495b57db5cf11a0e3cceb9692a575ced15d
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-27 17:37:57 +0200

    This would've worked if I knew Haskell.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,21 +4,27 @@ import XMonad.Util.EZConfig
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
 hypirionTerminal = "xfce4-terminal"
 
+hypirionWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 hypirionKeys (XConfig {modMask = mod4Mask}) = M.fromList $
-             [((mod4Mask, xK_F4), kill)]
+             [((mod4Mask, xK_F4), kill)] ++
+             [((m .|. mod4Mask, k), windows $ f i)
+                  | (i, k) <- zip hypirionWorkspaces [xK_1 .. xK_9]
+                  , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 hypirionLayout = tiled ||| Mirror tiled ||| noBorders Full
   where
     tiled = Tall 1 (3/100) (1/2)
 
-main = xmonad xfceConfig {
+main = xmonad $ xfceConfig {
      terminal = hypirionTerminal,
      modMask = mod4Mask,
      handleEventHook = fullscreenEventHook,
-     layoutHook =  avoidStruts $ hypirionLayout,
-     keys = \c -> hypirionKeys c `M.union` keys xfceConfig c
-     }
+     layoutHook = avoidStruts $ hypirionLayout,
+     workspaces = hypirionWorkspaces
+     } `additionalKeysP` hypirionKeys
